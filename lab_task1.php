198b1d58d2a830ed6b91f41c3339b22507e612bb
maruf-hasan7081 <marufhasanchowdhury06@gmail.com>
2025-07-25 22:06:53 +0600

    Initial commit

---
+++ lab_task1.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<html>
+    <title>this is registration form</title>
+<body>
+    <center>
+         <h1 style="color: blue;">AIUB</h1>
+        <h3 style="color: blue;">Registration From</h3>
+
+    </center>
+            <h3>Complete the Registration</h3>
+            <table>
+                <tr>
+                <td>Full Name:</td>
+                <td><input type="text" ></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type="email" ></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type="password" ></td>
+            </tr>
+            <tr>
+                <td>gender</td>
+                <td>
+                    <input type="radio" name="des"> male 
+                      <input type="radio" name="des"> female
+                </td>
+            </tr>
+            <tr>
+                <td>Language known</td>
+                <td>
+                    <input type="radio" name="des"> English 
+                     <input type="radio" name="des"> Bangla
+                     <input type="radio" name="des"> Hindi
+                </td>
+
+            </tr>
+            <tr>
+                <td>Country</td>
+                <td>
+                    <select name="country">
+                        <option value="Select">--Select--</option>
+                        <option value="Bangladesh">Bangladesh</option>
+                        <option value="India">India</option>
+                        <option value="Pakistan">Pakistan</option>
+                        <option value="Nepal">Nepal</option>
+                    </select>
+            </tr>
+            <tr>
+                <td>Date of Birth: </td>
+                <td><input type="date" ></td>
+        
+            </tr>
+            <tr>
+                <td>upload photo: </td>
+                <td><input type="file" ></td>
+
+            </tr>
+            <tr>
+                <td>comment:  </td>
+                <td><input type="text" ></td>
+            </tr>
+            <tr>
+                <td>
+                    
+                </td>
+                <td> 
+                    <input type="submit" value="Resister"> </td>
+            </tr>
+           
+
+     </table> 
+   
+     
+
+
+</body>
+</html>
