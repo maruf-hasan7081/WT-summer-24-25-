a4e3f9a727bef6646d2f63abde0c7e64e8c1efca
maruf-hasan7081 <marufhasanchowdhury06@gmail.com>
2025-08-03 06:07:44 +0600

    Add lab_task_4.php

---
+++ mid/mid/lab_task/lab_task_4.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login page </title>
+        <style>
+        table{
+            margin: 50px;
+            border: 50px;
+            padding: 150px;
+            background-color:rgb(43, 43, 42);
+        }
+        input[type="text"], input[type="password"] {
+            padding: 2px;
+            width: 150%;
+        }
+        input[type="submit"] {
+            padding: 10px;
+            width: 100%;
+            background-color: rgb(15, 15, 16);
+            color: rgb(99, 40, 40);
+            border: none;
+            
+        }
+       
+        </style>
+    </head>
+    <body style="background-color: black;">
+
+        <center>
+            <a href="html_images.asp" 
+target="_blank"
+>HTML Images</a>
+            
+        <table>
+           
+            <center>
+                <h1 style="color: white;">Login Form</h1>
+            </center>
+            <tr>
+                
+                <td style="color: white;"><b>Username:</b></td>
+                
+                </tr>
+            <tr>
+                
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                
+                <td style="color: white;"><b>Password:</b></td>
+            </tr>
+            <tr>
+                
+                <td><input type="password" ></td>
+            </tr>
+            <tr>
+                <td>
+
+                </td>
+                <td>
+                    <input type="submit" value="Login">
+                </td>
+            </tr>
+        </table>
+        </center>
+
+
+    </body>
+</html>
